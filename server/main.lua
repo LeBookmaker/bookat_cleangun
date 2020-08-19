37410d2577b7f98255b844d8bce469a3b7347104
Le Bookmaker <69488905+LeBookmaker@users.noreply.github.com>
2020-08-19 16:34:31 -0400

    Finish script
    :D

--- server/main.lua
+++ server/main.lua
@@ -1,23 +1,14 @@
 VORP = exports.vorp_inventory:vorp_inventoryApi()
 
+
 -- item for short weapon
 Citizen.CreateThread(function()
 	Citizen.Wait(2000)
 	VORP.RegisterUsableItem("cleanshort", function(data)
-	local _source = source
-		TriggerClientEvent('cleaning:startcleaningshort', data.source)
+		local _source = source
+		TriggerClientEvent('cleaning:startcleaningshort', data.source, cleaning)
 		VORP.subItem(data.source,"cleanshort", 1)
 	end)
 end)
-
----- item for long weapon
---Citizen.CreateThread(function()
---	Citizen.Wait(2000)
---	VORP.RegisterUsableItem("cleanlong", function(data)
---	local _source = source
---		TriggerClientEvent('cleaning:startcleaninglong', data.source)
---		VORP.subItem(data.source,"cleanlong", 1)
---	end)
---end)
 
 
